e8e18df82d6c3c029df1acb6473612af87fa12f4
afoninartem <afonin.art1988@gmail.com>
2023-09-29 12:51:15 +0300

    fix urls in dinner-store

---
+++ assets/AccessTestPage.a99acd0b.js
@@ -0,0 +1,1 @@
+import{_ as e,H as c,M as s}from"./index.069b876d.js";const t={};function r(a,n){return c(),s("h3",null,"Access test")}var _=e(t,[["render",r]]);export{_ as default};
---
+++ assets/CreateUser.86c4c5ea.js
@@ -0,0 +1,1 @@
+import{_ as e,H as r,M as s}from"./index.069b876d.js";const t={};function a(c,n){return r(),s("h3",null,"This is CreateUser")}var _=e(t,[["render",a]]);export{_ as default};
