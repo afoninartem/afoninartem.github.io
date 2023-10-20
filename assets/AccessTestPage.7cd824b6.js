1bcfd294d8d3b2da6799e787b3763c3faf119520
afoninartem <afonin.art1988@gmail.com>
2023-10-20 11:32:29 +0300

    fix upload btn

---
+++ assets/AccessTestPage.7cd824b6.js
@@ -0,0 +1,1 @@
+import{_ as e,H as c,M as s}from"./index.34ec4f39.js";const t={};function r(a,n){return c(),s("h3",null,"Access test")}var _=e(t,[["render",r]]);export{_ as default};
---
+++ assets/AdminPanel.b5319635.js
@@ -0,0 +1,1 @@
+import{_ as e,H as n,M as r}from"./index.34ec4f39.js";const a={};function c(s,t){return n(),r("h3",null,"This is AdminPanel")}var _=e(a,[["render",c]]);export{_ as default};
