a95de369d3cc8b453384a1f3ba6265081db8c96d
afoninartem <afonin.art1988@gmail.com>
2024-02-07 16:07:34 +0300

    fix loader on materials consumption page

---
+++ assets/ArtisLoader.0399dcbe.js
@@ -0,0 +1,1 @@
+import{Q as r}from"./QSpinnerCube.bbb26cc1.js";import{_ as e,H as o,I as c}from"./index.ddf01cde.js";const a={};function s(n,t){return o(),c(r,{color:"primary",size:"5.5em"})}var m=e(a,[["render",s]]);export{m as A};
