409c6e2e4b07c53b6dce527d7294d39073d622dd
afoninartem <afonin.art1988@gmail.com>
2024-03-05 10:07:03 +0300

    new pages: cashless, warehouse

---
+++ assets/ArtisLoader.cc5d94b8.js
@@ -0,0 +1,1 @@
+import{Q as r}from"./QSpinnerCube.06aee53b.js";import{_ as e,H as o,I as c}from"./index.ba59cbdf.js";const a={};function s(n,t){return o(),c(r,{color:"primary",size:"5.5em"})}var m=e(a,[["render",s]]);export{m as A};
---
+++ assets/use-quasar.42bd87d1.js
@@ -0,0 +1,1 @@
+import{x as a,aj as r}from"./index.ba59cbdf.js";function u(){return a(r)}export{u};
