20a304857053a4bdf03f34a8b30a49c939c3dd8d
afoninartem <afonin.art1988@gmail.com>
2024-05-21 15:36:06 +0300

    add manual for report 60

---
+++ assets/QCardSection.e1bfc9af.js
@@ -0,0 +1,1 @@
+import{h as e,k as r,u as n,x as s}from"./index.196e559a.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(a,{slots:t}){const o=r(()=>`q-card__section q-card__section--${a.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(a.tag,{class:o.value},s(t.default))}});export{i as Q};
---
+++ assets/use-quasar.8eba7352.js
@@ -0,0 +1,1 @@
+import{i as a,aj as r}from"./index.196e559a.js";function u(){return a(r)}export{u};
