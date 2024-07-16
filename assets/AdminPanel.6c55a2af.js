6124975c8093eb087d2ecff6ff9c11806eab2068
afoninartem <afonin.art1988@gmail.com>
2024-07-16 10:50:51 +0300

    fix print

---
+++ assets/AdminPanel.6c55a2af.js
@@ -0,0 +1,1 @@
+import{_ as e,p as n,y as r}from"./index.36d0b482.js";const a={};function c(s,t){return n(),r("h3",null,"This is AdminPanel")}var _=e(a,[["render",c]]);export{_ as default};
---
+++ assets/use-quasar.612c243c.js
@@ -0,0 +1,1 @@
+import{i as a,aj as r}from"./index.36d0b482.js";function u(){return a(r)}export{u};
