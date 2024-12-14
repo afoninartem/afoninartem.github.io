bb94e614d0b73f8145bb4ef53020f67ad2dc43d3
afoninartem <afonin.art1988@gmail.com>
2024-12-14 14:15:06 +0300

    base url change

---
+++ assets/AdminPanel.4f2c1d80.js
@@ -0,0 +1,1 @@
+import{_ as e,p as n,y as r}from"./index.ef225a1a.js";const a={};function c(s,t){return n(),r("h3",null,"This is AdminPanel")}var _=e(a,[["render",c]]);export{_ as default};
---
+++ assets/use-quasar.213a7c79.js
@@ -0,0 +1,1 @@
+import{i as a,aj as r}from"./index.ef225a1a.js";function u(){return a(r)}export{u};
