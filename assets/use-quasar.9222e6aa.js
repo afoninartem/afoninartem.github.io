8f0162413b3ca803826fd770a27e9a9883f5753e
afoninartem <afonin.art1988@gmail.com>
2025-03-11 12:28:27 +0300

    add wh needs for kristina

---
+++ assets/use-quasar.9222e6aa.js
@@ -0,0 +1,1 @@
+import{i as a,al as r}from"./index.07b77fcd.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar.cf7518a5.js
@@ -0,0 +1,1 @@
+import{i as a,al as r}from"./index.2510f71b.js";function u(){return a(r)}export{u};
