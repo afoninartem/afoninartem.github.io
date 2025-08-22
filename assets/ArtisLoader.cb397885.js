1c0f6dc94dc409a40d0a0aaf4b0c34fc5f4278a3
afoninartem <afonin.art1988@gmail.com>
2025-08-22 16:41:33 +0300

    add row nums in stock table

---
+++ assets/ArtisLoader.cb397885.js
@@ -0,0 +1,1 @@
+import{Q as r}from"./QSpinnerCube.1a2aace5.js";import{_ as e,p as o,q as c}from"./index.92b89367.js";const a={};function s(n,t){return o(),c(r,{color:"primary",size:"5.5em"})}var p=e(a,[["render",s]]);export{p as A};
---
+++ assets/use-quasar.6be9193d.js
@@ -0,0 +1,1 @@
+import{i as a,al as r}from"./index.92b89367.js";function u(){return a(r)}export{u};
