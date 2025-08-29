f146352732d6463c0399e1874d1ea156be0c3a5c
afoninartem <afonin.art1988@gmail.com>
2025-08-29 14:27:09 +0300

    update multiple files upload component

---
+++ assets/ArtisLoader.e2ac17ff.js
@@ -0,0 +1,1 @@
+import{Q as r}from"./QSpinnerCube.988a77e2.js";import{_ as e,p as o,q as c}from"./index.aac28b69.js";const a={};function s(n,t){return o(),c(r,{color:"primary",size:"5.5em"})}var p=e(a,[["render",s]]);export{p as A};
---
+++ assets/use-quasar.ca3fd056.js
@@ -0,0 +1,1 @@
+import{i as a,al as r}from"./index.aac28b69.js";function u(){return a(r)}export{u};
